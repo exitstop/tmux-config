7094b3070231eb5fca5a283c2f188274624db8f7
exitstop <exitstop@list.ru>
2020-06-12 22:37:01 +0300

    fix

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@
 
 # Steps to build and install tmux from source on Ubuntu.
 # Takes < 25 seconds on EC2 env [even on a low-end config instance].
-function IsPackageInstalled() {
-    dpkg -s "$1" > /dev/null 2>&1
-}
+#function IsPackageInstalled() {
+    #dpkg -s "$1" > /dev/null 2>&1
+#}
+#packages="tmux"
 
 ARG_1=$1
 if [ -z "$ARG_1" ]; then
@@ -14,29 +15,25 @@ if [ -z "$ARG_1" ]; then
 else
     echo "remove tmux";
     sudo apt-get -y remove tmux
+
+    VERSION=3.1
+    sudo apt-get -y install gcc wget tar libevent-dev libncurses-dev make
+    sudo apt-get install -f -y
+    sudo apt-get -y install wget tar libevent-dev libncurses-dev
+    wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+    tar xf tmux-${VERSION}.tar.gz
+    rm -f tmux-${VERSION}.tar.gz
+    cd tmux-${VERSION}
+    ./configure
+    make
+    sudo make install
+    cd -
+    sudo rm -rf /usr/local/src/tmux-*
+    sudo mv tmux-${VERSION} /usr/local/src
+
+    tmux kill-server
 fi
 
-packages="tmux"
-for package in $packages; do
-        if ! IsPackageInstalled $package; then
-                VERSION=3.1
-                sudo apt-get -y install gcc wget tar libevent-dev libncurses-dev make
-                sudo apt-get install -f -y
-                sudo apt-get -y install wget tar libevent-dev libncurses-dev
-                wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
-                tar xf tmux-${VERSION}.tar.gz
-                rm -f tmux-${VERSION}.tar.gz
-                cd tmux-${VERSION}
-                ./configure
-                make
-                sudo make install
-                cd -
-                sudo rm -rf /usr/local/src/tmux-*
-                sudo mv tmux-${VERSION} /usr/local/src
-
-                tmux kill-server
-        fi
-done
 
 
 ## Logout and login to the shell again and run.
@@ -47,26 +44,26 @@ set -u
 set -o pipefail
 
 is_app_installed() {
-  type "$1" &>/dev/null
+    type "$1" &>/dev/null
 }
 
 REPODIR="$(cd "$(dirname "$0")"; pwd -P)"
 cd "$REPODIR";
 
 if ! is_app_installed tmux; then
-  printf "WARNING: \"tmux\" command is not found. \
-Install it first\n"
-  exit 1
+    printf "WARNING: \"tmux\" command is not found. \
+        Install it first\n"
+    exit 1
 fi
 
 if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
-  printf "WARNING: Cannot found TPM (Tmux Plugin Manager) \
- at default location: \$HOME/.tmux/plugins/tpm.\n"
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    printf "WARNING: Cannot found TPM (Tmux Plugin Manager) \
+        at default location: \$HOME/.tmux/plugins/tpm.\n"
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 if [ -e "$HOME/.tmux.conf" ]; then
-  printf "Found existing .tmux.conf in your \$HOME directory. Will create a backup at $HOME/.tmux.conf.bak\n"
+    printf "Found existing .tmux.conf in your \$HOME directory. Will create a backup at $HOME/.tmux.conf.bak\n"
 fi
 
 cp -f "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak" 2>/dev/null || true
