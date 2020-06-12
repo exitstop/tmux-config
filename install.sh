307a8fb24bd86e25ab9b89ec41283435db0fbfb0
exitstop <exitstop@list.ru>
2020-06-12 15:25:40 +0300

    sudo apt-get install -f -y

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@
 VERSION=3.1
 sudo apt-get -y remove tmux
 sudo apt-get -y install wget tar libevent-dev libncurses-dev
+sudo apt-get install -f -y
+sudo apt-get -y install wget tar libevent-dev libncurses-dev
 wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
 tar xf tmux-${VERSION}.tar.gz
 rm -f tmux-${VERSION}.tar.gz
